22fb26076fc37debcd392548657f0e4442d53fcd
Prince kumar <122357150+itzprincephutela@users.noreply.github.com>
2024-02-05 02:38:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Youtuber" ,"Student","Coder"],
+    typeSpeed: 100,       // Customized typing speed in milliseconds
+    backSpeed: 100,       // Customized backspacing speed in milliseconds
+    backDelay: 1000,     // Customized delay before starting to backspace
+    loop: true,          // Whether to loop the animation or not
+    showCursor: false,   // Whether to show the blinking cursor or not
+    cursorChar: "|",     // Customized cursor character
+  });
